fbdcf615adce80b5c9782b322e0f20c5b69fc3b3
toino <joaoapereira21@hotmail.com>
2022-09-06 23:53:49 +0100

    Add solutions to the day 4 puzzles

---
+++ day04/part2.py
@@ -0,0 +1,27 @@
+import itertools as it
+import more_itertools as mit
+import functools as ft
+import operator as op
+import re
+from collections import Counter, defaultdict
+
+with open("input") as inf, open("part2.out", "w+") as outf:
+    pattern = re.compile(r"\[\d{4}-\d{2}-\d{2} \d{2}:(\d{2})] (?:Guard #(\d+) begins shift|(wakes up)|(falls asleep))")
+    l = sorted(inf)
+    c = Counter()
+    id = -1
+    start = -1
+
+    for i in l:
+        match pattern.match(i).groups():
+            case [mins, None, None, "falls asleep"]:
+                start = int(mins)
+            case [mins, None, "wakes up", None]:
+                for m in range(start, int(mins)):
+                    c[id, m] += 1
+            case [_, _id, None, None]:
+                id = int(_id)
+
+    id, m = c.most_common(1)[0][0]
+
+    outf.write(str(id * m))
